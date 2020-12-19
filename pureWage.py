98ec0a9d6522a18916571f46b51596feacf10950
goldenwo <68965162+goldenwo@users.noreply.github.com>
2020-12-18 23:18:57 -0500

    Initial inputs for login details

--- pureWage.py
+++ pureWage.py
@@ -3,4 +3,42 @@ from requests import get
 from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
+import json
 
+login_info = {}
+with open("loginInfo.json") as f:
+  login_info = json.load(f)
+
+headers = {
+    'authority': 'www.purewage.com',
+    'cache-control': 'max-age=0',
+    'sec-ch-ua': '"Google Chrome";v="87", " Not;A Brand";v="99", "Chromium";v="87"',
+    'sec-ch-ua-mobile': '?0',
+    'origin': 'https://www.purewage.com',
+    'upgrade-insecure-requests': '1',
+    'dnt': '1',
+    'content-type': 'application/x-www-form-urlencoded',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-user': '?1',
+    'sec-fetch-dest': 'document',
+    'referer': 'https://www.purewage.com/',
+    'accept-language': 'en-US,en;q=0.9',
+    'cookie': '__cfduid=d9a8d3b1aa6573e08f3430ce18ff94dbf1608325317',
+}
+
+data = {
+  'account': login_info["username"],
+  'password': login_info["password"],
+  'btn_login': 'Login',
+  'IdBook': '33,42,147,151'
+}
+
+
+url = "https://www.purewage.com/login.aspx"
+
+s = requests.Session()
+
+response = s.post(url, headers=headers, data=data)
---
+++ scraper.py
@@ -0,0 +1,66 @@
+import os
+import json
+
+data = {}
+filename = "loginInfo.json"
+supported_sites = ["purewage.com", "ibet.ag", "everysport247.com", "betallweek.com"]
+
+def generate_login_info():
+    # purewage.com
+    purewage_username = input("Please enter your username for purewage.com: ")
+    purewage_password = input("Please enter your password for purewage.com: ")
+    purewage_dict = {"username":purewage_username, "password":purewage_password}
+    data["purewage.com"] = purewage_dict
+    print()
+    # ibet.ag
+    ibet_username = input("Please enter you username for ibet.ag: ")
+    ibet_password = input("Please enter you password for ibet.ag: ")
+    ibet_dict = {"username":ibet_username, "password":ibet_password}
+    data["ibet.ag"] = ibet_dict
+    print()
+    # everysport247.com
+    everysport247_username = input("Please enter you username for everysport247.com: ")
+    everysport247_password = input("Please enter you password for everysport247.com: ")
+    everysport247_dict = {"username":everysport247_username, "password":everysport247_password}
+    data["everysport247.com"] = everysport247_dict
+    print()
+    # betallweek.com
+    betallweek_username = input("Please enter you username for betallweek.com: ")
+    betallweek_password = input("Please enter you password for betallweek.com: ")
+    betallweek_dict = {"username":betallweek_username, "password":betallweek_password}
+    data["betallweek.com"] = betallweek_dict
+    print()
+    # Creates JSON file
+    with open(filename, "w") as fp:
+        json.dump(data, fp)
+
+# Script starts here
+print("Sports Betting Scraper v1.0\n")
+print("Sites supported: ")
+for site in supported_sites:
+    print(site)
+print()
+
+# If loginInfo.json exists
+if (os.path.exists(filename) and os.path.getsize(filename) > 0):
+    # If you want to make edits to login info
+    while True:
+        edits = input("Would you like to edit any of your login information? [Y/N]: ")
+        print()
+        if ("y" == edits.lower()):
+            generate_login_info()
+            break
+        elif("n" == edits.lower()):
+            break
+
+    # Copies info from loginInfo.json into data dictionary
+    with open(filename) as login_info:
+        data = json.load(login_info)
+
+
+# Else generate json file with login info
+else:
+    generate_login_info()
+
+
+input("Press enter to exit\n")
